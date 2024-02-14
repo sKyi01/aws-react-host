614a24afc17719554f63f6de78af3b243fca6131
sKyi01 <113761204+sKyi01@users.noreply.github.com>
2024-02-14 17:52:17 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1> AWS React Host Application</h1>
+      <h1> Azure React Host Application</h1>
     </div>
   );
 }
